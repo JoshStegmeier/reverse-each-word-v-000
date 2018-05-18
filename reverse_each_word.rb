0e02589e0fd024f29bd209e2dd938fd4617501f4
Josh Stegmeier <josh@steg.me>
2018-05-18 19:18:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,5 @@
 def reverse_each_word(str)
-  newArray = str.split(/ /)
-  newArray.each do | word |
-    word.reverse!
-  end
-  return newArray
-end
-
-test:
-
-def reverse_each_word
-  sentence = "Hello there, and how are you?"
-  newArray = sentence.split
+  newArray = str.split
   newArray.each do | word |
     word.reverse!
   end
